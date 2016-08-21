71ca003764308eb1e212dc33c4b5abf3ca4fd088
Topher Sheridan <topher@xobanimot.com>
2016-08-22 08:19:12 +1000

    Added custom LogLevel strings

--- POGOLib/Logging/Logger.cs
+++ POGOLib/Logging/Logger.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Runtime.Remoting.Channels;
 
 namespace POGOLib.Logging
 {
     public static class Logger
     {
+        public static void Custom(LogLevel logLevel, string customLogLevel, string message, params object[] args)
+        {
+            Output(logLevel, string.Format(message, args), customLogLevel);
+        }
+
 		public static void Debug(string message, params object[] args)
         {
 			Output(LogLevel.Debug, string.Format(message, args));
@@ -29,13 +35,14 @@ namespace POGOLib.Logging
 			Output(LogLevel.Error, string.Format(message, args));
         }
 
-        private static void Output(LogLevel logLevel, string message)
+        private static void Output(LogLevel logLevel, string message, string customLogLevel = null)
         {
             if (logLevel < LoggerConfiguration.MinimumLogLevel) return;
 
             var foregroundColor = LoggerConfiguration.DefaultForegroundColor;
             var backgroundColor = LoggerConfiguration.DefaultBackgroundColor;
             var timestamp = DateTime.Now.ToString("HH:mm:ss");
+            var logLevelText = customLogLevel ?? logLevel.ToString();
 
             if (LoggerConfiguration.LogLevelColors.ContainsKey(logLevel))
             {
@@ -47,7 +54,7 @@ namespace POGOLib.Logging
 
             Console.ForegroundColor = foregroundColor;
             Console.BackgroundColor = backgroundColor;
-			Console.WriteLine(string.Format("{0,-10}{1,-8}{2}", timestamp, logLevel, message));
+			Console.WriteLine(string.Format("{0,-10}{1,-10}{2}", timestamp, logLevelText, message));
             Console.ResetColor();
         }
     }
